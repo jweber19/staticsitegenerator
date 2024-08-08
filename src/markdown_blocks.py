dc2d690d11508d2e5babfb4ad7f348f3c5ac8a63
Josiah Weber <97723871+jweber19@users.noreply.github.com>
2024-08-08 16:45:59 +0900

    markdown blocks 1st iteration

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
 def markdown_to_blocks(markdown):
-    markdown += "\nHello, I am not a line of text. I am an imposter"
-    return markdown
+    temp_string = ""
+    blocks = []
+
+    parts = markdown.split('\n')
+    if part in parts != "":
+      temp_string += (f"{part}"'\n')
+      else:
+            block_list.append(temp_string)
+            temp_string = ""
+    if temp_string:
+      block_list.append(temp_string.strip())
+    return block_list
