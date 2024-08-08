325e59ebe0cc20b0c5e562e316e8631e87335203
Josiah Weber <97723871+jweber19@users.noreply.github.com>
2024-08-08 17:45:41 +0900

    markdown blocks v.02

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -4,10 +4,9 @@ def markdown_to_blocks(markdown):
 
     parts = markdown.split('\n')
     if part in parts != "":
-      temp_string += (f"{part}"'\n')
-      else:
-            block_list.append(temp_string)
+        temp_string += (f"{part}"'\n')
+    else:
+        if temp_string.strip():
+            blocks.append(temp_string.strip())
             temp_string = ""
-    if temp_string:
-      block_list.append(temp_string.strip())
-    return block_list
+    return blocks
