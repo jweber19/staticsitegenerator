88f575a28e1aa620dcc3746ce799a78e231a9ae9
jweber19 <jtw497@gmail.com>
2024-08-15 20:00:41 +0900

    block type v.01

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,4 @@
+import re
 def markdown_to_blocks(markdown):
     temp_string = ""
     blocks = []
@@ -11,3 +12,18 @@ def markdown_to_blocks(markdown):
                 blocks.append(temp_string.strip())
                 temp_string = ""
     return blocks
+
+def block_to_block_type(markdown):
+    if markdown == re.match(r"(#+)('')(.*?)", markdown):
+        return "heading"
+    #if markdown == x:
+        #return "code"
+    #if markdown == x:
+        #return "quote"
+    #if markdown == x:
+        #return "unordered_list"
+    #if markdown == x:
+        #return "ordered_list"
+    else:
+        return "paragraph"
+    
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type
 
 class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_simple(self):
@@ -45,3 +45,15 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+    def test_blocktype_heading(self):
+        md = "# This Is a Heading"
+        block_type = block_to_block_type(md)
+        #print(block_type)
+        self.assertEqual(block_type, "heading")
+    
+    def test_blocktype_paragraph(self):
+        md = "This is **bolded** paragraph"
+        block_type = block_to_block_type(md)
+        #print(block_type)
+        self.assertEqual(block_type, "paragraph")
