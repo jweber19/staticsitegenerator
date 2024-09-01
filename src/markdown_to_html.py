fdb2d0717d4982ff9fa707917fe9a3a463cb3b81
jweber19 <jtw497@gmail.com>
2024-09-01 20:46:03 +0900

    paragraph leafnodes now nest as children in a parentnode

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -30,20 +30,36 @@ block_type_ordered_list = "ordered list"
 block_type_quote = "quote"
 
 
+"""
+class TestMarkdownToHTML(unittest.TestCase):
+    def test_markdown_block_to_html_node(self):
+        md = '''
+# GitHub
+
+It's a **great** website where you can effectively manage *version control*
+
+## Also
+
+- It's easy to use
+- Super popular
+
+[Check out my GitHub page](https://github.com/jweber19)
+'''
+        markdown_to_html_node(md)
+"""
+
 def block_to_textnodes(block_type, block):
     if block_type == block_type_paragraph: # process paragraph blocks
-        textnodes = text_to_textnodes(block)
-        """
-        for node in textnodes:
-            print(node) # debug output
-            Output:
-TextNode(It's a , text, None)
-TextNode(great, bold, None)
-TextNode( website where you can effectively manage , text, None)
-TextNode(version control, italic, None)
-TextNode(Check out my GitHub page, link, https://github.com/jweber19)
-            """
-        return textnodes
+        children = [] # set up list to hold leafnodes
+        textnodes = text_to_textnodes(block) # convert from text to textnodes
+
+        for node in textnodes:  # loop over each textnodes list
+            leafnode = text_node_to_html_node(node) # convert each textnode to an html leafnode. recursive function for children?
+            children.append(leafnode) # append leafnode to child list
+        parentnode = ParentNode("p", children) # set up parentnode object and inject child list
+    
+        return parentnode
+    
     if block_type == block_type_heading: # process heading blocks
         pass
     if block_type == block_type_unordered_list: # process unordered list blocks
@@ -56,33 +72,10 @@ TextNode(Check out my GitHub page, link, https://github.com/jweber19)
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown) # separate md string block into \n separated blocks
-    #print("\n") # debugging
-    textnode_count = 0 # used to count block iterations for clearer debugging output formatting
-    print('\n') # debug output formatting
+    print('\n')
     for block in blocks: # iterate over block list
         block_type = block_to_block_type(block) # get block type from markdown_blocks
-        textnodes = block_to_textnodes(block_type, block) # convert blocks using if switches to list of textnodes (see above function)
-
-        if textnodes is not None:   # skip empty blocks, temporary: change once all if switches are complete
-            textnode_count += 1 # used to count block iterations for clearer debugging output formatting
-            print(textnode_count) # used to count block iterations for clearer debugging output formatting
-
-            for node in textnodes:  # loop over each textnodes list
-                htmlnode = text_node_to_html_node(node)     # convert each textnode to an html node. recursive function for children?
-                print(htmlnode) # debugging
-            print('\n') # used to separate iterations for clearer debugging output
-
-            """Output:
-
-1
-LeafNode(None, It's a , None)
-LeafNode(b, great, None)
-LeafNode(None,  website where you can effectively manage , None)
-LeafNode(i, version control, None)
-
-
-2
-LeafNode(a, Check out my GitHub page, {'href': 'https://github.com/jweber19'})
-
-
-"""
+        htmlnode = block_to_textnodes(block_type, block) # convert blocks using if switches to list of textnodes (see above function)
+        if htmlnode is not None:   # skip empty blocks, temporary: change once all if switches are complete
+            print(f"{type(htmlnode)}'\n'") # debug
+            print(htmlnode.__repr__) # debug
