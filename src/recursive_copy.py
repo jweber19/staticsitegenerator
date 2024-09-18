9ebea0c7e84f25e524e14d2e5eb25ebaa79a133d
jweber19 <jtw497@gmail.com>
2024-09-18 23:55:02 +0900

    recursive_copy src file started

---
+++ src/recursive_copy.py
@@ -0,0 +1,27 @@
+import os
+
+path = "test_path_dir"
+
+def main():
+    #if os.path.exists(path): # verify if a string exists as a path in the cd
+    #    print(f"success! {path}")
+    #else:
+    #    print("fail")
+
+    #if os.listdir(path): # return any directories as a string
+    #    dir = os.listdir(path)
+    #    path_list = []
+    #    for d in dir:
+    #        new_path = ""
+    #        new_path = os.path.join(path, d) # concatenate two strings into a path separated by a '/'
+    #        path_list.append(new_path)
+    #print(path_list)
+    #else:
+    #    print("no directories found")
+    #print(os.path.isfile(path)) # return true if the path variable is a file
+    #print(os.path.isdir(path)) # return true if the path variable is a folder
+    #os.mkdir(path) # creates a directory in the path
+    #shutil.copy(dst, src) # copy files from a source to a destination
+    #shutil.rmtree # delete an entire directory tree - path must point to a directory
+    pass
+main()
