16370d1bf6f64eae63396a8b11dc3e44f5f5e8a0
jweber19 <jtw497@gmail.com>
2024-09-20 00:19:58 +0900

    Recursive file copy function complete.

--- src/recursive_copy.py
+++ src/recursive_copy.py
@@ -1,65 +1,49 @@
 import os, shutil
 
-#path = "test_path_dir"
-source_path = "static"
-dest_path = "public"
-
-def main():
-    #if os.path.exists(path): # verify if a string exists as a path in the cd
-    #    print(f"success! {path}")
-    #else:
-    #    print("fail")
-
-    #if os.listdir(path): # return any directories as a string
-    #    dir = os.listdir(path)
-    #    path_list = []
-    #    for d in dir:
-    #        new_path = ""
-    #        new_path = os.path.join(path, d) # concatenate two strings into a path separated by a '/'
-    #        path_list.append(new_path)
-    #print(path_list)
-    #else:
-    #    print("no directories found")
-    
-    #print(os.path.isfile(path)) # return true if the path variable is a file
-    #print(os.path.isdir(path)) # return true if the path variable is a folder
-    
-    #os.mkdir(path) # creates a directory in the path
-    
-    #shutil.copy(dst, src) # copy files from a source to a destination
-    #shutil.rmtree # delete an entire directory tree - path must point to a directory
-    
-    # check if public path exists
-    # if yes shutil.rmtree public path
-    # else return error: destination path not found
-
-    # check if static path exists
-    # if yes copy files from static path to public path
-        # for each file print each path to the screen
-    # else return error source path not found
-    
-    print(f"Cleaning destination path '{dest_path}'")
-    shutil.rmtree(dest_path)
-    os.mkdir(dest_path)
-    print("\nComplete.")
-    input("Check if public folder cleaned.\nPress any key to continue...")
-    print(f"\nCopying from local source folder '{source_path}' to local destination folder '{dest_path}'.")
-    os.listdir(source_path)
-    dir = os.listdir(source_path)
-    for d in dir:
+# recursively get file list, copy file if file, if folder create folder and repeat from start
+def recursive_folder_copy(source_path, dest_path):
+    files = os.listdir(source_path)
+    for file in files:
         source_content_path = ""
         dest_content_path = ""
-        source_content_path = os.path.join(source_path, d)
-        dest_content_path = os.path.join(dest_path, d)
-        # if source content path is folder
-            # if not folder dest content path (folder source content path)
-                # mkdir in dest content path
-        # if source content path is file
-            # if not file dest content path (file source content path)
-                # shutil.copy
+        source_content_path = os.path.join(source_path, file)
+        dest_content_path = os.path.join(dest_path, file)
+        if os.path.isfile(source_content_path):
+            print(f"Copying file: '{source_content_path}' to '{dest_path}'.")
+            shutil.copy(source_content_path, dest_content_path)
+        else:
+            os.mkdir(dest_content_path)
+            recursive_folder_copy(source_content_path, dest_content_path)
 
-        print(f"\nCopying... {source_content_path}")
-        shutil.copy(dest_content_path, source_content_path)
-    print("Complete.")
+# remove and recreate empty destination path if it exists otherwise create one
+def folder_clean(path):
+    print(f"Cleaning '{path}'.")
+    shutil.rmtree(path) # remove folder and subdirs
+    print(f"Verifying destination folder removal...")
+    if not os.path.exists(path): # verify folder does not exist
+        print("Destination folder removal successful.\nCreating new empty folder.")
+        os.mkdir(path) # create the folder
+        print("Folder created.")
+    else:
+        raise Exception("Folder was not deleted.\nCanceling operation.")
 
-main()
+# verify folder presences, raise errors, debug and run functions.
+def recursive_copy(source_path, dest_path):
+    print("Initiating folder cleaner...")
+    if os.path.exists(dest_path):
+        folder_clean(dest_path) # folder cleaner function
+    else:
+        print(f"Directory '{dest_path}' does not exist.\nCreating empty directory '{dest_path}'...")
+        os.mkdir(dest_path)
+    print("Cleaning complete.\n")
+    print("Verifying source folder...")
+    if os.path.exists(source_path):
+        print("Source folder exists. Verification successful.\n")
+        print("Initiating file copy...")
+        recursive_folder_copy(source_path, dest_path) # copy function
+        print("File copy complete.")
+    else:
+        raise Exception(f"Source directory '{source_path}' missing.\nCanceling operation.")
+    
+    
+    print("Operation complete.")
