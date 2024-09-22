864000500590abd0420cd534df788e035de53be1
jweber19 <jtw497@gmail.com>
2024-09-22 16:05:22 +0900

    Change main.py to call page_generator and build site file.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+from page_generator import generate_page
 from recursive_copy import recursive_copy
 
 def main():
     source_path = "static"
     dest_path = "public"
+    from_path = "content/index.md"
+    template_path = "template.html"
+
     recursive_copy(source_path, dest_path)
-    
+    generate_page(from_path, dest_path, template_path)
+
 main()
