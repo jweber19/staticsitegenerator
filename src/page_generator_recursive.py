4cc51af6a7e9250c23c8c75774c9be4ec994aeb4
jweber19 <jtw497@gmail.com>
2024-09-23 13:21:10 +0900

    Add initial page generator function(non functional).

---
+++ src/page_generator_recursive.py
@@ -0,0 +1,57 @@
+import os
+from markdown_to_html import markdown_to_html_node
+from extract_title import extract_title
+
+def generate_pages_recursive(from_path, template_path, dest_path):
+    # get content directory file list and crawl each folder for markdown files.
+    # for each markdown file found, generate a new .html file using the template.html
+        # the generated pages should be written to the public folder in the same directory structure
+    # change the main function to use the recursive version so that all md files in the source path are recursively constructed.
+
+    current_directory = os.getcwd()
+    from_path_full = os.path.join(current_directory, from_path)
+    template_path_full = os.path.join(current_directory, template_path)
+    dest_path_full = os.path.join(current_directory, dest_path, "index.html")
+    dest_path_folder = os.path.join(current_directory, dest_path)
+    if os.path.exists(from_path_full):
+        print("From path found.")
+    else:
+        raise Exception(f"Error. Missing '{from_path}'.")
+    if os.path.exists(dest_path_folder):
+        print("Destination path found.")
+    else:
+        #print(f"'{dest_path}' does not exist. Creating folder...")
+        os.mkdir(template_path_full)
+        #print("Done.")
+    if os.path.exists(template_path_full):
+        print("Template path found.")
+    else:
+        raise Exception(f"Error. Missing '{template_path}'.")
+
+    # gather necessary files for html generation
+    #print("\nReading data...")
+    with open(from_path_full, 'r') as f:
+        markdown = f.read()
+    with open(template_path_full, 'r') as f:
+        #print("Success.")
+        template_html = f.read()
+    #print("Converting Markdown to HTML..")
+    node = markdown_to_html_node(markdown)
+    #print("Success.")
+    #print("Extracting title...")
+    title = extract_title(markdown)
+    #print("Success.")
+    #print("Generating HTML...")
+    generated_html = node.to_html()
+    #print("Done.")
+    
+    # inject generated html into template
+    #print("Creating HTML file...")
+    output_html = template_html.replace("{{ Title }}", title)
+    output_html = output_html.replace("{{ Content }}", generated_html)
+    #print("Done.")
+    #print("Writing to file...")
+    with open(dest_path_full, 'w') as f:
+        f.write(output_html)
+        #print("Finished.")
+    #print("Operation completed.")
