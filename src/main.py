70837616eeffba5e2a1a1147079be1c917f97ac6
jweber19 <jtw497@gmail.com>
2024-09-28 19:27:15 +0900

    Update main.py to use new recursive ver of page generator function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
-from page_generator import generate_page
+from page_generator_recursive import generate_page_recursive
 from recursive_copy import recursive_copy
 
 def main():
     source_path = "static" # important files c
     dest_path = "public" # output folder
-
     from_path = "content" # markdown content
     template_path = "template.html" # html template for generator
 
-    recursive_copy(source_path, dest_path) # cleans dest_path folder and recursively copies source_path contents for use in generator
-    generate_page(from_path, dest_path, template_path) # generates html using template in from_path
+    recursive_copy(source_path, dest_path)
+    generate_page_recursive(from_path, dest_path, template_path)
     
 main()
